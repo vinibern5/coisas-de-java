1b03d481dfd46e8c56baadca07f13216efe7b38d
Vinicius Bernardo <vinibern5@gmail.com>
2017-04-12 22:20:57 -0300

    Create Aluno.java

---
+++ homeworks/ListaAlunos/Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno {
+	private String nome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String toString() {
+		return this.nome;
+	}
+	public boolean equals(Object o) {
+		Aluno outro = (Aluno)o;
+		return this.nome.equals(outro.nome);
+	}
+}
