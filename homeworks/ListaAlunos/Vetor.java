f5307f7e7cc3975b928d504a3c76163aac74315a
Vinicius Bernardo <vinibern5@gmail.com>
2017-04-12 22:21:24 -0300

    Create Vetor.java

---
+++ homeworks/ListaAlunos/Vetor.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Vetor {
+	private Aluno[] alunos = new Aluno[10];
+	public void adiciona(Aluno aluno) {
+    for(int i=0;i<alunos.length;i++){
+      if (alunos[i] == null){
+        alunos[i] = aluno;
+        break;
+       }
+      }
+        
+    }
+	
+	public void adiciona(int posicao, Aluno aluno) {
+    if (alunos[posicao] != null){
+      System.out.printf("Erro, posição já foi ocupada!");
+    }
+    else{
+      alunos[posicao] = aluno;
+    }
+	}
+	public Aluno pega(int posicao) {
+    return null;
+	}
+	public void remove(int posicao) {
+	  return;
+	}
+	public boolean contem(Aluno aluno) {
+    return false;
+	}
+	public int tamanho() {
+    return 0;
+	}
+	public String toString() {
+		return Arrays.toString(alunos);
+	}
+}
