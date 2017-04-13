7d101fa0302ab8924023d9be477bda7062183c4d
Vinicius Bernardo <vinibern5@gmail.com>
2017-04-12 22:20:01 -0300

    Create main.java

---
+++ homeworks/ListaAlunos/main.java
@@ -0,0 +1,12 @@
+public class Main{
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno();
+		a1.setNome("João");
+		a2.setNome("José");
+		Vetor lista = new Vetor();
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+		System.out.println(lista);
+	}
+}
