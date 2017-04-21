756243ef663d592bf704e72d61e2080dc1a0043d
Vinicius Bernardo <vinibern5@gmail.com>
2017-04-20 23:27:32 -0300

    Indent fixes

--- homeworks/ListaAlunos/main.java
+++ homeworks/ListaAlunos/main.java
@@ -2,29 +2,29 @@ public class Main{
 	public static void main(String[] args) {
 	  //Testes -- Usei printf pra facilitar na hora de ler as saídas no console
 	  
-		Aluno a1 = new Aluno();
-		Aluno a2 = new Aluno();
-		Aluno a3 = new Aluno();
-		Aluno a4 = new Aluno();
-		Aluno a5 = new Aluno();
-		a1.setNome("João");
-		a2.setNome("José");
-		a3.setNome("Bernardo");
-		a4.setNome("Vinicius");
-		a5.setNome("Coelhinho da Pascoa");
-		Vetor lista = new Vetor();
-		System.out.println("Size: " + lista.tamanho() + '\n'); //Quantidade de elementos na nossa lista!
-		lista.adiciona(a1);
-		lista.adiciona(a2);
-		System.out.println("Size: " + lista.tamanho() + '\n'); //Quantidade de elementos na nossa lista!
-		lista.adiciona(0,a3); //Não é possível, pois a posição 0 já foi alocada 
-		System.out.printf("%s \n\n",lista); //Mostrar a lista
-		lista.adiciona(3,a4);
-		System.out.printf("%s \n\n",lista); //Mostrar a lista
-		lista.adiciona(a5); //Será adicionado na posição 2, pois a mesma está vazia lista[2] == null //
-		System.out.printf("%s \n\n",lista); //Mostrar a lista
-		System.out.printf("%s \n\n",lista.pega(0)); //Mostra o que está na posição "0"
-		System.out.printf("%s \n\n",lista.pega(2)); //Posição "2"
+	  Aluno a1 = new Aluno();
+	  Aluno a2 = new Aluno();
+	  Aluno a3 = new Aluno();
+	  Aluno a4 = new Aluno();
+	  Aluno a5 = new Aluno();
+	  a1.setNome("João");
+	  a2.setNome("José");
+	  a3.setNome("Bernardo");
+	  a4.setNome("Vinicius");
+	  a5.setNome("Coelhinho da Pascoa");
+	  Vetor lista = new Vetor();
+	  System.out.println("Size: " + lista.tamanho() + '\n'); //Quantidade de elementos na nossa lista!
+	  lista.adiciona(a1);
+	  lista.adiciona(a2);
+	  System.out.println("Size: " + lista.tamanho() + '\n'); //Quantidade de elementos na nossa lista!
+	  lista.adiciona(0,a3); //Não é possível, pois a posição 0 já foi alocada 
+	  System.out.printf("%s \n\n",lista); //Mostrar a lista
+	  lista.adiciona(3,a4);
+	  System.out.printf("%s \n\n",lista); //Mostrar a lista
+	  lista.adiciona(a5); //Será adicionado na posição 2, pois a mesma está vazia lista[2] == null //
+	  System.out.printf("%s \n\n",lista); //Mostrar a lista
+	  System.out.printf("%s \n\n",lista.pega(0)); //Mostra o que está na posição "0"
+	  System.out.printf("%s \n\n",lista.pega(2)); //Posição "2"
 	  System.out.printf("%s \n\n",lista.pega(7));
 	  lista.remove(0); //Remove o elemento que está na posição 0 -> [null,José,Coelhinho,...]
 	  System.out.printf("%s \n\n",lista.pega(0)); //Mostra o que está na posição 0 agora
